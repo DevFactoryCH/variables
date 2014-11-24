76ca2770bd9a746d1d90bc943cb5d001f259b728
Mark Cameron <mark.cameron@devfactory.ch>
2014-11-24 17:07:26 +0100

    Updated config variables.php with an example, and left it empty

--- src/config/variables.php
+++ src/config/variables.php
@@ -1,10 +1,23 @@
 <?php
 
-return array(
+/**
+ * Items added to the array should look like this:
+ *
+ *   'key' => array(
+ *     'value' => '',
+ *     'translation_key' => 'variables.key',
+ *   ),
+ *
+ * Which might give something like:
+ *
+ *   'service_api_key' => array(
+ *     'value' => 'da46f8af58aec448c784dd421660f7635d404feb',
+ *     'translation_key' => 'API Key for Service',
+ *   ),
+ *
+ * @return
+ */
 
-  'test' => array(
-    'value' => 'abcdef',
-    'translation_key' => 'variables.whatwhat',
-  ),
+return array(
 
 );
