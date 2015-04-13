cb27704531bf05214b6f15b61d5910559406952e
Mark Cameron <mark.cameron@devfactory.ch>
2015-04-13 14:21:46 +0200

    Updated variables config with fixed hepl text and example data

--- src/config/variables.php
+++ src/config/variables.php
@@ -5,19 +5,22 @@
  *
  *   'key' => array(
  *     'value' => '',
- *     'translation_key' => 'variables.key',
+ *     'translation_key' => 'variables.key', // Create in your language files
  *   ),
  *
  * Which might give something like:
  *
  *   'service_api_key' => array(
  *     'value' => 'da46f8af58aec448c784dd421660f7635d404feb',
- *     'translation_key' => 'API Key for Service',
+ *     'translation_key' => 'variables.service_api_key',
  *   ),
  *
  * @return
  */
 
 return array(
-
+  'example' => array(
+    'value' => 'Example data!',
+    'translation_key' => 'variables.example',
+  ),
 );
